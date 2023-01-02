39644dbf2908142fdd62bae917efa9dacc50773a
nolanzhong <nolan.zhong@duke.edu>
2023-01-02 11:43:08 -0500

    completed timer

--- TimerProject/ViewController.swift
+++ TimerProject/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer = Timer()
+    var counter = 0
 
+    @IBOutlet weak var timeLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counter = 10
+        timeLable.text = "Time: \(counter)"
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(timerFunc), userInfo: nil, repeats: true)
+    }
+    
+    @objc func timerFunc() {
+        
+        timeLable.text = "Time: \(counter)"
+        counter -= 1
+        
+        if counter == 0 {
+            timer.invalidate()
+            timeLable.text = "Timer Done"
+        }
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("button clicked")
+    }
+    
 }
 
