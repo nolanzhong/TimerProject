97d1b8c3c53595c122a536ff22d47b4ebb79dfaf
nolanzhong <nolan.zhong@duke.edu>
2023-01-02 11:31:27 -0500

    Initial Commit

---
+++ TimerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by Nolan Zhong on 1/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
